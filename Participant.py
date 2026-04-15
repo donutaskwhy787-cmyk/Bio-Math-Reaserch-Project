b0fe1e9fde62a9219148f610f979c8096076b66c
Michael <donutaskwhy787@gmail.com>
2026-04-15 13:49:36 -0400

    constructor added
    auto formats into dict strings from the input

--- Participant.py
+++ Participant.py
@@ -1,1 +1,13 @@
+import numpy as np
 
+class Participant:
+  def __init__(self, headers, values):
+    hds = headers.split(" ")
+    vals = values.split(" ")
+    self.values = dict()
+
+    for i in range(len(hds)):
+      self.values[hds[i]] = vals[i]
+
+
+    
