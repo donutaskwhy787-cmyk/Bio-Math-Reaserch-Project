7560ee8d1c4f9be07724333d4f5102608583ba48
Michael <donutaskwhy787@gmail.com>
2026-04-17 14:44:36 -0400

    Refactor Participant class initialization and add deepcopy
    Refactor Participant class to initialize values separately and add deepcopy method.

--- Participant.py
+++ Participant.py
@@ -1,7 +1,9 @@
 import numpy as np
 #idh 1 = mutant
 class Participant:
-  def __init__(self, headers, values):
+  def __init__(self):
+    self.values = dict()
+  def initalize(self, headers, values):
     hds = headers.split(" ")
     vals = values.split(" ")
     self.values = dict()
@@ -9,5 +11,12 @@ class Participant:
     for i in range(len(hds)):
       self.values[hds[i]] = vals[i]
 
+  def deepcopy(self):
+    c = dict()
+    p = Participant()
+    for i in self.values:
+      c[i] = self.values[i]
+    p.values = c
+    return p
 
     
